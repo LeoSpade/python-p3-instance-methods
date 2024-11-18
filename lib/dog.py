a6ec61b7d1ae4024a89d5650cee0faa33c410e8d
thornm <atmaystar@gmail.com>
2024-11-18 15:20:30 -0500

    instance methods lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 class Dog:
+
+    def __init__(self):
+        pass
     # Class body goes here
 
     #Instance method definition
+
+    def bark(self):
+        self.bark = "Woof!"
+        print("Woof!")
+
+    def sit(self):
+        self.sit = "The dog is sitting."
+        print("The dog is sitting.")
+
     pass
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 class Person:
+
+    def __init__(self):
+        pass
     # Class body goes here
+
+    def walk(self):
+        # self.walk = "The person is walking."
+        print("The person is walking.")
+
+    def talk(self):
+        # self.talk = "Hello World!"
+        print("Hello World!")
 
     #Instance method definition
     pass
